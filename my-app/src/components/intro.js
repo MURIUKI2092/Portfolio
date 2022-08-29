9c820f30b24b36c05486fd837db11d110c51dc94
OchiengHosea <jrhosea@gmail.com>
2022-08-29 12:06:35 +0300

    fixed #0004

--- my-app/src/components/intro.js
+++ my-app/src/components/intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
       <div className='introDetails'>
       <span  className='introName'><h3>JAMES MURIUKI</h3></span>
       <span className="introRole"><p>Full Stack Software Developer</p></span>
-      <span className='desc'><p> Hi, Am a Junior Software Engineer with a deeper passion in writing codes to assist and solve others problems. Skilled in Javascript, ReactJs, Node, PostgreSQL, MongoDB,
+      <span className='desc'><p> Hi, Am a Junior Software Engineer with a deeper passion in writing codes to assist and solve others problems. Skilled in Javascript, ReactJs, NodeJs,  MongoDB,PostgreSQL,
       Cascading Style Sheets (CSS) and HTML,Python
       , Django and Flask framework,.</p></span>
       </div>
